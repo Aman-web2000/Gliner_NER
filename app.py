0299ba0567b1a076d88f0ec93b4aa2676bfea3e6
Aman Chauhan <amanchauhan2@deloitte.com>
2024-08-26 01:13:07 +0530

    removed a model

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("GliNer - NER Model")
 st.write("Named Entity Recognition using gliner""")
 
 text=st.text_input("Enter the Text", key="Text Box")
-model_version=st.selectbox("Select a gliner model",["urchade/gliner_mediumv2.1","urchade/gliner_multi_pii-v1"])
+model_version=st.selectbox("Select a gliner model",["urchade/gliner_mediumv2.1"])
 
 if st.button("run"):
     entities=ner_model(text, model_version=model_version)
